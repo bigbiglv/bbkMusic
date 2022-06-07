27db5596ec9e3799b18531c9b607a00bb1a5a7c3
bigbigking <2845758942@qq.com>
2022-06-07 16:20:36 +0800

    feat:获取audio对象

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -3,11 +3,14 @@ import { ref, onMounted } from 'vue'
 
 const audioSrc = ref('http://music.163.com/song/media/outer/url?id=413859.mp3')
 onMounted(() => {
+  const appAudio = document.getElementById('appAudio') as HTMLAudioElement
+  appAudio.src = audioSrc.value || ''
+  console.log(appAudio)
 })
 </script>
 
 <template>
-  <audio ref="appAudio" controls  ></audio>
+  <audio id="appAudio" controls  />
 </template>
 
 
