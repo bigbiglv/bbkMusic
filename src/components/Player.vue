8186057643287b276af567082f9851e9c035fed6
bigbigking <2845758942@qq.com>
2022-06-08 10:09:10 +0800

    feat:audiostore中添加播放暂停方法

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
 import audioStore from '@/store/audioStore'
 const store = audioStore()
-console.log(store)
+let { audioEl } = storeToRefs(store)
 
 
 const audioSrc = ref('http://music.163.com/song/media/outer/url?id=413859.mp3')
 onMounted(() => {
-  const appAudio = document.getElementById('appAudio') as HTMLAudioElement
-  store.audioEl = appAudio
-  appAudio.src = audioSrc.value || ''
-  console.log(store)
+  audioEl.value = document.getElementById('appAudio') as HTMLAudioElement
+
+  audioEl.value.src = audioSrc.value || ''
 })
 </script>
 
--- src/store/audioStore.ts
+++ src/store/audioStore.ts
@@ -3,11 +3,30 @@ import { defineStore } from 'pinia'
 export default defineStore('audioStore', {
   state: () => {
     return {
-      audioEl: null,
+      audioEl: null as HTMLAudioElement | null,
     }
   },
   getters: {
+    //当前播放的链接
+    audioSrc(state){
+      return state.audioEl?.src
+    },
+    isPaused(state){
+      return state.audioEl?.paused
+    }
   },
   actions:{
+    //播放音频
+    play(){
+      if(this.audioEl){
+        this.audioEl.play()
+      }
+    },
+    //暂停音频
+    pause(){
+      if(this.audioEl){
+        this.audioEl.pause()
+      }
+    }
   }
 })
