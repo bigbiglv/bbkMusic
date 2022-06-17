31200125b3a3ce889da71d0292f962594e995761
bigbigking <2845758942@qq.com>
2022-06-17 16:47:43 +0800

    fix:touch和click的冲突

--- src/hooks/useTouch.ts
+++ src/hooks/useTouch.ts
@@ -13,20 +13,24 @@ export default function useTouch(el: HTMLElement) {
   //touch的状态
   const isTouch = ref(false);
   el?.addEventListener('touchstart', (e:TouchEvent)=>{
-    // e.preventDefault()
     startX.value = e.touches[0].clientX;
     startY.value = e.touches[0].clientY;
   })
   el?.addEventListener('touchmove', (e)=>{
-    //偏移量大于20才算滑动
+    
     let X = e.touches[0].clientX - startX.value;
     let Y = e.touches[0].clientY - startY.value;
     if(isTouch.value) {
+      //超出当前元素的范围阻止默认事件会报错
+      if(e.cancelable) e.preventDefault() 
       x.value = X
       y.value = Y
       return
     }
-    if(Math.abs(X) > 20 || Math.abs(Y) > 20) isTouch.value = true;
+    //偏移量大于20才算滑动 事实上这里是先执行的
+    if(Math.abs(X) > 20 || Math.abs(Y) > 20) {
+      isTouch.value = true;
+    }
   })
   el?.addEventListener('touchend', ()=>{
     startX.value = 0;
