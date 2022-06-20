99dd67b161356d009f8c8734f42b2195a4ba8dc2
bigbigking <2845758942@qq.com>
2022-06-20 09:14:26 +0800

    feat:播放列表

--- src/components/Lyrics/LyricsPlayBar.vue
+++ src/components/Lyrics/LyricsPlayBar.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import audioStore from '@/store/audioStore';
-
+import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
 const storeAudio = audioStore();
-const { showTime, showDuration, duration, progress, isPause } = storeToRefs(storeAudio)
+const { showTime, showDuration, duration, progress, isPause, playList, currentIndex } = storeToRefs(storeAudio)
 function dragEnd(){
   storeAudio.isDrag = false
   storeAudio.setProgress(progress.value)
 }
+
+const isShowList = ref(false)
 </script>
 
 <template>
@@ -33,6 +35,22 @@ function dragEnd(){
     <icon-ic-twotone-pause-circle-filled @click="storeAudio.pause" v-show="!isPause"/>
     
     <icon-ic-twotone-skip-next @click="storeAudio.next"/>
+
+    <icon-icon-park-twotone-music-list @click="isShowList = true"/> 
+
+    <van-popup 
+      v-model:show="isShowList"
+      position="bottom"
+      :style="{ height: '50%' }"
+    >
+      <van-cell 
+        v-for="(song,index) in playList" 
+        :key="song['id']" 
+        :title="song['name']" 
+      >
+      <icon-icon-park-twotone-play v-if="currentIndex === index"/>
+      </van-cell>
+    </van-popup>
   </div>
 </template>
 
