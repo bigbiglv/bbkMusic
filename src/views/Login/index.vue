44c7fbfbcc41b13852395a008d802cb554007d61
bigbigking <2845758942@qq.com>
2022-06-21 17:04:22 +0800

    feat:登录接口

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,56 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { Login } from '@/api'
+const router = useRouter()
 
+
+const username = ref('bigbigking')
+const password = ref('123456')
+const loading = ref(false)
+async function onSubmit(){
+  loading.value = true
+  try{
+    const { result } = await Login({username:username.value,password:password.value})
+    console.log(result)
+    localStorage.setItem('token',result.token)
+    router.replace('/')
+
+  }catch(error){
+    console.log(error)
+  }
+  loading.value = false
+}
 </script>
 
 <template>
-  <div>
-    <router-link to="/">login</router-link>
+  <div class="w-screen h-screen flex items-center justify-center">
+    <div class=" pt-5 rounded-sm shadow-lg">
+      <van-form @submit="onSubmit">
+        <van-cell-group inset>
+          <van-field
+            v-model="username"
+            name="用户名"
+            label="用户名"
+            placeholder="用户名"
+            :rules="[{ required: true, message: '请填写用户名' }]"
+          />
+          <van-field
+            v-model="password"
+            type="password"
+            name="密码"
+            label="密码"
+            placeholder="密码"
+            :rules="[{ required: true, message: '请填写密码' }]"
+          />
+        </van-cell-group>
+        <div style="margin: 16px;">
+          <van-button round block type="primary" native-type="submit" :loading="loading">
+            登录
+          </van-button>
+        </div>
+      </van-form>
+    </div>
   </div>
 </template>
 
