c083e0eee77ec8fe0e5b41f7e149c784057e89ea
bigbigking <2845758942@qq.com>
2022-06-22 10:21:24 +0800

    feat:全局引入vant样式

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from './router'
 import { createPinia } from 'pinia'
 //index.css
 import './index.css'
+import 'vant/lib/index.css'
 const app = createApp(App) 
 app.use(createPinia())
 app.use(router)
