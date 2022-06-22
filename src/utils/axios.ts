6189b9a9acf4ce14f95e94a67d51bb7c110b1e2e
bigbigking <2845758942@qq.com>
2022-06-22 10:24:32 +0800

    feat:请求信息添加弹出提示

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,16 @@
 import axios, {AxiosInstance, AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios'
+import { Toast } from 'vant'
 // import {ElMessage} from 'element-plus'
 // 数据返回的接口
 // 定义请求响应参数，不含data
 interface Result {
-  code: number;
+  code: number
   msg: string
 }
 
 // 请求响应参数，包含data
 interface ResultData<T = any> extends Result {
-  data?: T;
+  data?: T
 }
 const URL: string = '/api'
 enum RequestEnums {
@@ -29,10 +30,10 @@ const config = {
 
 class RequestHttp {
   // 定义成员变量并指定类型
-  service: AxiosInstance;
+  service: AxiosInstance
   public constructor(config: AxiosRequestConfig) {
     // 实例化axios
-    this.service = axios.create(config);
+    this.service = axios.create(config)
 
     /**
       * 请求拦截器
@@ -41,7 +42,7 @@ class RequestHttp {
       */
     this.service.interceptors.request.use(
        (config: AxiosRequestConfig) => {
-         const token = localStorage.getItem('token') || '';
+         const token = localStorage.getItem('token') || ''
          return {
             ...config,
             headers: {
@@ -61,60 +62,59 @@ class RequestHttp {
       */
     this.service.interceptors.response.use(
        (response: AxiosResponse) => {
-         const {data, config} = response; // 解构
-         if (data.code === RequestEnums.OVERDUE) {
-            // 登录信息失效，应跳转到登录页面，并清空本地的token
-            localStorage.setItem('token', '');
-            console.log('登录失效')
-            return Promise.reject(data);
-         }
-         // 全局错误信息拦截（防止下载文件得时候返回数据流，没有code，直接报错）
-         if (data.code && data.code !== RequestEnums.SUCCESS) {
-            // ElMessage.error(data); // 此处也可以使用组件提示报错信息
-            return Promise.reject(data)
-         }
-         return data;
+          const {data, config} = response // 解构
+          if (data.code === RequestEnums.OVERDUE) {
+             // 登录信息失效，应跳转到登录页面，并清空本地的token
+             localStorage.setItem('token', '')
+             Toast.fail('登录信息已失效，请重新登录')
+             return Promise.reject(data)
+          }
+          // 全局错误信息拦截（防止下载文件得时候返回数据流，没有code，直接报错）
+          if (data.code && data.code !== RequestEnums.SUCCESS) {
+             // ElMessage.error(data) // 此处也可以使用组件提示报错信息
+             return Promise.reject(data)
+          }
+          return data
        },
        (error: AxiosError) => {
-         const {response} = error;
+         const {response} = error
          if (response) {
             this.handleCode(response.status)
          }
          if (!window.navigator.onLine) {
-            // ElMessage.error('网络连接失败');
-            // 可以跳转到错误页面，也可以不做操作
+            Toast.fail('网络连接失败')
             // return router.replace({
             //   path: '/404'
-            // });
+            // })
          }
        }
     )
   }
   handleCode(code: number):void {
     switch(code) {
-       case 401:
-        //  ElMessage.error('登录失败，请重新登录');
-         break;
-       default:
-        //  ElMessage.error('请求失败');
-         break;
+      case 401:
+        Toast.fail('登录失效，请重新登录')
+        break
+      default:
+        Toast.fail('请求失败')
+        break
     }
   }
 
   // 常用方法封装
   get<T>(url: string, params?: object): Promise<ResultData<T>> {
-    return this.service.get(url, {params});
+    return this.service.get(url, {params})
   }
   post<T>(url: string, params?: object): Promise<ResultData<T>> {
-    return this.service.post(url, params);
+    return this.service.post(url, params)
   }
   put<T>(url: string, params?: object): Promise<ResultData<T>> {
-    return this.service.put(url, params);
+    return this.service.put(url, params)
   }
   delete<T>(url: string, params?: object): Promise<ResultData<T>> {
-    return this.service.delete(url, {params});
+    return this.service.delete(url, {params})
   }
 }
 
 // 导出一个实例对象
-export default new RequestHttp(config);
+export default new RequestHttp(config)
